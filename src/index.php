4fd5ee13c007c687cee26cb3aa65580cb26660b2
y_yamaguchi <chrisrerei125815@gmail.com>
2023-06-17 13:29:53 +0000

    データベース名変更

--- src/index.php
+++ src/index.php
@@ -6,7 +6,7 @@
 </head>
 <body>
     <?php
-        $dsn = 'mysql:dbname=test;host=db';
+        $dsn = 'mysql:dbname=travelblog;host=db';
         $user = 'root';
         $password = 'pass';
         $pdo = new PDO($dsn, $user, $password, array(PDO::ATTR_ERRMODE => PDO::ERRMODE_WARNING));
